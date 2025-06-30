d37a88ae3b362541dc27eed22a327b925f22912c
Louise Aldridge <louisealdridge88@gmail.com>
2025-06-30 13:34:02 +0100

    challenge 4 complete

---
+++ challenge-4/index.js
@@ -0,0 +1,16 @@
+function checkOddOrEven(num){
+
+    if(num % 2 === 0){
+        console.log(num,"is even")
+    } else {
+        console.log(num, "is odd")
+    }
+}
+
+checkOddOrEven(15)
+checkOddOrEven(12)
+checkOddOrEven(2)
+
+
+//Check if a number is even or odd using an if-else statement.
+
