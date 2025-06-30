e709c8592799110716c6fbafbbc22c402dab79ef
Louise Aldridge <louisealdridge88@gmail.com>
2025-06-30 14:16:54 +0100

    Challenge 7 complete

---
+++ challenge-7/index.js
@@ -0,0 +1,10 @@
+const food = ["Pizza", "Chicken", "Ramen", "Chilli", "Potatoes"]
+
+for(let i = 0; i <= food.length -1; i++ ){
+    console.log(food[i])
+}
+
+
+
+//Create an array of five items and print each one using a for loop.
+
