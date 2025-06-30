c5210a42d770e87ff726ca6ccf8fe4245deef51c
Louise Aldridge <louisealdridge88@gmail.com>
2025-06-30 15:38:49 +0100

    challenge 9 complete

---
+++ challenge-9/index.js
@@ -0,0 +1,13 @@
+function reverseString(string){
+
+const reversedWord = string.split("").reverse().join("")
+console.log(reversedWord)
+
+}
+
+reverseString("Louise")
+reverseString("Cat")
+reverseString("Dog")
+
+// Write a function that reverses a string passed to it.
+
