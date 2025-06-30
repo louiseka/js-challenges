fa0bee550dca6ef095cd9b2cb87b120ac1458d02
Louise Aldridge <louisealdridge88@gmail.com>
2025-06-30 13:26:43 +0100

    challenge 3 complete

---
+++ challenge-3/index.js
@@ -0,0 +1,17 @@
+let num1 = 3
+let num2 = 15
+
+const sum = num1 + num2
+const difference = num1 - num2
+const product = num1 * num2
+const quotient = num1 / num2
+
+console.log("Sum:", sum)
+console.log("Difference", difference)
+console.log("Product:", product)
+console.log("Quotient:", quotient)
+
+
+
+
+//Write a program that takes two numbers and outputs their sum, difference, product, and quotient.
