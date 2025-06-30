6d51e483380b17d3401cf17684056708f452fa66
Louise Aldridge <louisealdridge88@gmail.com>
2025-06-30 15:38:58 +0100

    challenge 10 complete

---
+++ challenge-10/index.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "Louise",
+    age: 29,
+    hobbies: ["Coding", "Baking", "Crocheting"]
+}
+
+console.log(person)
+
+
+
+//Create an object representing a person with name, age, and hobby properties. Print them to the console.
+
