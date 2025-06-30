16fa042344ec70d6d09b42cb8038f56226d9d7f1
Louise Aldridge <louisealdridge88@gmail.com>
2025-06-30 12:08:14 +0100

    challenge 2

---
+++ challenge-2/index.js
@@ -0,0 +1,17 @@
+const number = 1
+const string = "Hello"
+const boolean = true
+const nullVar = null
+const undefinedVar = undefined
+const array = []
+const object = {}
+
+console.log(number, string, boolean, nullVar, undefinedVar, array, object)
+
+
+
+
+
+
+// Challenge:
+// Declare variables of different types: string, number, boolean, null, undefined, array, and object. Log them to the console.
