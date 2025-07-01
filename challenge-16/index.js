709c4fbb19dfed0b167dca312bed3c446d445104
Louise Aldridge <louisealdridge88@gmail.com>
2025-07-01 14:16:13 +0100

    challenge 16 complete

---
+++ challenge-16/index.js
@@ -0,0 +1,19 @@
+
+for (let i = 1; i < 101; i++){
+     if(i % 5 === 0 & i % 3 === 0){
+        console.log(i, "FizzBuzz")
+    }
+    else if(i % 3 === 0){
+        console.log(i, "Fizz")
+    } 
+    else if(i % 5 === 0){
+        console.log(i, "Buzz")
+    }
+    else {
+        console.log(i)
+    }
+}
+
+
+// Print numbers 1–100. For multiples of 3, print “Fizz”; for 5, “Buzz”; and for both, “FizzBuzz”.
+
