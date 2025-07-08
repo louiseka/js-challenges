359a5e8342003a51d5d7a5975db6220a880f37f5
Louise Aldridge <louisealdridge88@gmail.com>
2025-07-08 14:53:22 +0100

    challenge 22 complete

---
+++ challenge-22/index.js
@@ -0,0 +1,7 @@
+window.localStorage.setItem('saved-string', "Hello there")
+
+const savedString = window.localStorage.getItem('saved-string')
+
+console.log(savedString)
+
+//Store a string value in localStorage and retrieve it.
