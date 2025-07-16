173cd9d009434df44e513326476af375746ae556
Louise Aldridge <louisealdridge88@gmail.com>
2025-07-16 14:51:34 +0100

    challenge 31 complete

---
+++ challenge-31/index.js
@@ -0,0 +1,10 @@
+function findLargest(arr) {
+    return Math.max(...arr)
+}
+
+console.log(findLargest([6, 2, 3, 4]))
+console.log(findLargest([-1, -2, -3]))
+
+
+
+//Write a function that returns the largest number in a given array
